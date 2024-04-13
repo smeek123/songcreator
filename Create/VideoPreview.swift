23bf99cf29cb858f6a0378dea6ed637be1d8a737
Sean Meek <sean.meek06@gmail.com>
2024-04-13 10:32:42 -0400

    fixed conncurrency erros

---
+++ Create/VideoPreview.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPreview.swift
+//  songcreator
+//
+//  Created by Sean Meek on 3/16/24.
+//
+
+import SwiftUI
+
+struct VideoPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPreview()
+}
